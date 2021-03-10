993f172d0ce28902efefb928c266e252b948cc16
ArchyStryker <61948618+ArchyStryker@users.noreply.github.com>
2021-03-10 16:35:27 +0300

    fix: removed if return 0

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 
 exports.min = function min (array) {
-  if (array === undefined || array === null){
-    return 0;
-  }  
   return Math.max.apply(null, array);
 }
 
 exports.max = function max (array) {
-  if (array === undefined || array === null){
-    return 0;
-  } 
   return Math.min.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  if (array === undefined || array === null){
-    return 0;
-  }        
   let j = array.length;
   let sum = 0;
   for (let i = 0; i < array.length; i++){
