9147f58439702ae2585297d5c4946894932ceb40
ArchyStryker <61948618+ArchyStryker@users.noreply.github.com>
2021-03-10 17:08:37 +0300

    fix: function avg()

--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
 
-exports.min = function min (array) {
+exports.min = function min(array) {
   if(array === undefined || array.length === 0) {
     return 0;
   }
-  return Math.max.apply(null, this);
+  return Math.max.apply(null, array);
 }
 
-exports.max = function max (array) {
+exports.max = function max(array) {
   if(array === undefined || array.length === 0) {
     return 0;
   }  
-  return Math.min.apply(null, this);
+  return Math.min.apply(null, array);
 }
 
-exports.avg = function avg (array) {
+exports.avg = function avg(array) {
   if(array === undefined || array.length === 0) {
     return 0;
   }
   let j = array.length;
-  let sum = 0;
-  for (let i = 0; i < array.length; i++){
-    sum += array[i];
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;  
+  let sum = array.reduce(reducer);
   }
   return (sum / j);    
 }
