e350fe9a440897aa12cfd31c81c8cb2196ce8428
ArchyStryker <61948618+ArchyStryker@users.noreply.github.com>
2021-03-10 16:52:16 +0300

    fix: added if return 0

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 
 exports.min = function min (array) {
+  if(array === undefined || array.length === 0) {
+    return 0;
+  }
   return Math.max.apply(null, array);
 }
 
 exports.max = function max (array) {
+  if(array === undefined || array.length === 0) {
+    return 0;
+  }  
   return Math.min.apply(null, array);
 }
 
 exports.avg = function avg (array) {
+  if(array === undefined || array.length === 0) {
+    return 0;
+  }
   let j = array.length;
   let sum = 0;
   for (let i = 0; i < array.length; i++){
